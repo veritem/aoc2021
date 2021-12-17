120d37b4106a48c5b3733742f4b333b3f6e7e62d
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-12-17 11:01:00 +0200

    feat: init

---
+++ day0/target/debug/day0.d
@@ -0,0 +1,1 @@
+/home/veritem/dev/aoc/day0/target/debug/day0: /home/veritem/dev/aoc/day0/src/main.rs
---
+++ day0/target/debug/deps/day0-cbde13d1a553a9b0.d
@@ -0,0 +1,5 @@
+/home/veritem/dev/aoc/day0/target/debug/deps/day0-cbde13d1a553a9b0: src/main.rs
+
+/home/veritem/dev/aoc/day0/target/debug/deps/day0-cbde13d1a553a9b0.d: src/main.rs
+
+src/main.rs:
