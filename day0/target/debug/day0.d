d80cb355f67901f9c926544e93bcf094ba6ad312
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-12-17 11:02:50 +0200

    chore: initial commit

--- day0/target/debug/day0.d
+++
@@ -1,1 +0,0 @@
-/home/veritem/dev/aoc/day0/target/debug/day0: /home/veritem/dev/aoc/day0/src/main.rs
--- day0/target/debug/deps/day0-cbde13d1a553a9b0.d
+++
@@ -1,5 +0,0 @@
-/home/veritem/dev/aoc/day0/target/debug/deps/day0-cbde13d1a553a9b0: src/main.rs
-
-/home/veritem/dev/aoc/day0/target/debug/deps/day0-cbde13d1a553a9b0.d: src/main.rs
-
-src/main.rs:
